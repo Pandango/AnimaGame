50626918f20b2b987649705f0d28e203d87272ce
Pandango <littlepanda.daisuki@gmail.com>
2017-04-23 17:06:22 +0700

    Add Utilites

---
+++ Anima/Assets/Scripts/Utilities/JsonHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.ClassObject
+{
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string FormatJsonArrayItems(string json)
+        {
+            return "{\"Items\":" + json + "}";
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+}
