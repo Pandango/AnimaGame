117368d1ecb33887a192a8350361dde02ef110d3
Pandango <littlepanda.daisuki@gmail.com>
2017-04-25 02:00:25 +0700

    Add GameResource Model

---
+++ Anima/Assets/Scripts/Models/GameResourceDataModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResourceDataModel : MonoBehaviour {
+    public static PopulationFoodBalanced PopulationFood { get; set; }
+
+    public static SharingResource SharingResources;
+
+    public static BuildingResource BuildingResouces;
+
+    public static NaturalResource NaturalResources;
+}
+
+[Serializable]
+public class PopulationFoodBalanced
+{
+    public int population;
+    public int food;
+}
+
+[Serializable]
+public class SharingResource
+{
+    public int wood;
+    public int stone;
+}
+
+[Serializable]
+public class BuildingResource
+{
+    public int WoodCutterExp;
+    public int MineExp;
+    public int FarmExp;
+    public int TownExp;
+}
+
+[Serializable]
+public class NaturalResource
+{
+    public int wateLv;
+    public int forestExp;
+}
