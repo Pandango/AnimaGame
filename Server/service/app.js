24bc7d771ef94a5c124966271c602d2f3b255884
Pandango <littlepanda.daisuki@gmail.com>
2017-04-25 00:12:51 +0700

    workaround Server service

--- Server/service/app.js
+++ Server/service/app.js
@@ -2,19 +2,72 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var gameRouters = require('./gameRouters');
+// var gameRouters = require('./gameRouters');
 
 var server = require("http").createServer(app);
 var io = require('socket.io')(server);
+var sockets;
 
 app.set('port', process.env.PORT || 8080);
 
 var clients = [];
+var ingame_client = [];
 
-io.sockets.on('connection', function (socket){
-    gameRouters.initGame(io, socket);
+io.on('connection', function (socket){
+    var currentUser;
 
+    socket.on("login", function(data){
+        if(clients.length < 4){
+            currentUser = {
+                'username' : data.username,
+                'state': data.state
+            }
+            //add user in server
+            clients.push(currentUser);
+            
+            socket.emit("user_connected", currentUser);
+            io.sockets.emit("current_client", {clients});
+            console.log("Username " + currentUser.username + " is connected");
+        }else{
+            message = "Room is full!"
+            socket.emit("user_unconnected", message);
+        }
+    });
+
+    socket.on("current_client", function() {
+        socket.emit("current_client", {clients});
+    });
     
+    socket.on("update_userstate", function(data){
+        //role 
+        clients[data.role].state = data.state;
+
+        io.sockets.emit("current_client", {clients});
+        console.log("Username " + clients[data.role].username + " is " + clients[data.role].state);
+    });
+
+    //game play socket
+    // socket.on("start_game", function(playerRegisterData){
+    //     var regis_player = {
+    //         'username' : playerRegisterData.username,
+    //         'role' : playerRegisterData.role,
+    //         'score' : 0
+    //     }
+
+    //     ingame_client.push[regis_player];
+    //     io.sockets.emit("start_game", {ingame_client});
+    // });
+
+    socket.on("disconnect", function (){
+        socket.broadcast.emit("user_disconnect", currentUser);      
+        for(var userCouter = 0; userCouter < clients.length; userCouter++){
+            if(clients[userCouter].username === currentUser.username){
+                console.log("User " + clients[userCouter].username + " disconnected");
+                clients.splice(userCouter, 1);
+            }
+        }
+        io.sockets.emit("current_client", {clients});
+    })
 });
 
 server.listen(app.get('port'), function () {
--- Server/service/gameRouters.js
+++ Server/service/gameRouters.js
@@ -1,19 +1,22 @@
 var io;
 var gameSocket;
 var currentUser;
-var app = require('./app');
+var clients;
 
-exports.initGame = function (sio, socket){
+var gameRouters = module.exports = {};
+
+gameRouters.initGame = function (sio, socket, client){
     io = sio;
     gameSocket = socket;
+    clients = client;
 
     socket.on("login", createJoinedUser);
     socket.on("current_client", updateClient);
 
-    socket.on("disconnet", onDisconnect);
+    socket.on("disconnect", onDisconnect);
 }
 
-function createJoinedUser(userdata) {
+function createJoinedUser(data) {
     var newUser = {
         'username' : data.username,
         'state' : data.state
@@ -21,13 +24,14 @@ function createJoinedUser(userdata) {
 
     currentUser = newUser;
 
-    app.clients.push(newUser);
-    io.sockets.emit("CURRENT_CLIENT", {clients});
+    clients.push(newUser);
+    gameSocket.emit(currentUser);
+    // io.sockets.emit("current_client", app.clients);
     console.log("Username " + newUser.username + " is connected");
 }
 
 function updateClient() {
-    socket.emit("current_client", {client});
+    gameSocket.emit("current_client", {client});
 }
 
 function updateUserState(userData) {
@@ -40,12 +44,12 @@ function onUpdateUsetState(data){
      //role 
         clients[data.role].state = data.state;
 
-        io.sockets.emit("CURRENT_CLIENT", {clients});
+        io.sockets.emit("current_client", {clients});
         console.log("Username " + clients[data.role].username + " is " + clients[data.role].state);
 }
 
 function onDisconnect(){
-    socket.broadcast.emit("USER_DISCONNECTED", currentUser);
+    gameSocket.broadcast.emit("user_disconnected", currentUser);
     for(var userCouter = 0; userCouter < clients.length; userCouter++){
         if(clients[userCouter].username === currentUser.username){
             console.log("User " + clients[userCouter].username + " disconnected");
