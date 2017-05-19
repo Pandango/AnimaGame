cd7ba8528701e187b6e3a9959435a7590fa171f2
Pandango <littlepanda.daisuki@gmail.com>
2017-05-20 04:29:45 +0700

    Add back to main menu btn

---
+++ Anima/Assets/Scripts/Controller/OnBackToMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnBackToMenuController : MonoBehaviour {
+
+    public void OnBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+
+        if(GameObject.Find("SocketIO") != null)
+        {
+            Destroy(GameObject.Find("SocketIO"));
+        }
+    }
+}
