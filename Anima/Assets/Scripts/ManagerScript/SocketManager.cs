8e8cdebaad892b0b775faf2309369b6d8f718229
Pandango <littlepanda.daisuki@gmail.com>
2017-05-24 01:11:00 +0700

    Add feature back to login when disconnect

--- Anima/Assets/Scripts/ManagerScript/SocketManager.cs
+++ Anima/Assets/Scripts/ManagerScript/SocketManager.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
+﻿using SocketIO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SocketManager : MonoBehaviour {
+    private SocketIOComponent _socket;
+    public GameObject DisconnectPanel;
 
-	void Awake()
+    public GameObject loginPanel;
+    public GameObject lobbyPanel;
+
+
+    void Awake()
     {
+        DisconnectPanel.SetActive(false);
+        _socket = this.gameObject.GetComponent<SocketIOComponent>();
         DontDestroyOnLoad(transform.gameObject);
+    }
 
-        //if (GameObject.Find("SocketIO") != null)
-        //{
-        //    Destroy(this.gameObject);
-        //}
+    void Start()
+    {
+        _socket.On("disconnect", OnServerDown);
+    }
+
+    void OnServerDown(SocketIOEvent evt)
+    { 
+        DisconnectPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        Scene activeScene = SceneManager.GetActiveScene();
+        if (activeScene.name != "MainMenu")
+        {
+            SceneManager.LoadScene("MainMenu");
+
+            if (GameObject.Find("SocketIO") != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            loginPanel.SetActive(true);
+            lobbyPanel.SetActive(false);
+        }
+        DisconnectPanel.SetActive(false);
     }
 }
